d80e603e0cc921ce6cbbc18c56eacd8ffe4715fa
dercioink <dercionhatsave@gmail.com>
2021-04-05 20:57:31 +0200

    UI start page

--- src/pages/principal/principal.ts
+++ src/pages/principal/principal.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+import { RegistoDTO } from '../../models/registo.dto';
+import { RegistoService } from '../../services/domain/registo.service';
+import { StorageService } from '../../services/storage.service';
 
 /**
  * Generated class for the PrincipalPage page.
@@ -15,7 +18,34 @@ import { IonicPage } from 'ionic-angular';
 })
 export class PrincipalPage {
 
-  constructor() {
+  utilizador: RegistoDTO[];
+
+  constructor(
+    public navCtrl: NavController,
+    public storage: StorageService,
+    public registoService: RegistoService) {
+  }
+
+  ionViewDidLoad() {
+    let localUser = this.storage.getLocalUser();
+      if(localUser && localUser.contacto){
+        this.registoService.findByContacto(localUser.contacto)
+        .subscribe(response => {
+          this.utilizador = response;
+        },
+        error=>{
+          if(error.status == 403){
+            // this.navCtrl.setRoot('HomePage');
+          }
+        });
+        
+      }else{
+          // this.navCtrl.setRoot('HomePage');
+      }
+  }
+
+  registar(){
+    this.navCtrl.push('RegistarPage');
   }
 
 }
